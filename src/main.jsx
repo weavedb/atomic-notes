8df23955442c5172769afdcebdc1a1107400c8ab
Tomoya Nagasawa <ocrybit@gmail.com>
2024-07-11 23:25:13 +0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { ChakraProvider } from "@chakra-ui/react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
