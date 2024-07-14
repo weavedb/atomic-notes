e73d684e566e75173fee030b8c35452163ee260b
Tomoya Nagasawa <ocrybit@gmail.com>
2024-07-14 15:55:33 +0400

    add hash routing & article page

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,47 @@ import { useState } from "react"
 import "./App.css"
 import tomo from "/tomo.png"
 import { Image, Flex, Box } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 
 function App() {
   const url = new URL(location.href)
   const image = url.origin + "/cover.png"
   return (
-    <>
-      <Flex align="center" justify="center" h="100%">
-        <Box textAlign="center" pb={6}>
-          <Flex justify="center">
-            <Image src={tomo} boxSize="200px" />
+    <Flex justify="center">
+      <Flex direction="column" h="100%" w="100%" maxW="830px">
+        <Flex align="center" p={6}>
+          <Flex justify="center" mr={4}>
+            <Image src={tomo} boxSize="100px" />
           </Flex>
-          <Box fontSize="75px" fontWeight="bold">
-            TOMO
+          <Box>
+            <Box fontSize="25px" fontWeight="bold">
+              TOMO
+            </Box>
+            <Box fontSize="16px" mb={2}>
+              Permaweb Hacker
+            </Box>
+            <Box mr={2} as="a" target="_blank" href="https://x.com/0xTomo">
+              <Box fontSize="25px" as="i" className="fab fa-twitter" />
+            </Box>
+            <Box
+              mr={2}
+              as="a"
+              target="_blank"
+              href="https://github.com/ocrybit"
+            >
+              <Box fontSize="25px" as="i" className="fab fa-github" />
+            </Box>
           </Box>
-          <Box fontSize="25px" mb={4}>
-            Permaweb Hacker
-          </Box>
-          <Box mx={2} as="a" target="_blank" href="https://x.com/0xTomo">
-            <Box fontSize="30px" as="i" className="fab fa-twitter" />
-          </Box>
-          <Box mx={2} as="a" target="_blank" href="https://github.com/ocrybit">
-            <Box fontSize="30px" as="i" className="fab fa-github" />
+        </Flex>
+        <Box w="100%" flex={1} style={{ borderTop: "1px solid #333" }}>
+          <Box p={4} fontSize="20px">
+            <Link to="./a/1">
+              <Box as="u">Deploy PermaApp on Arweave</Box>
+            </Link>
           </Box>
         </Box>
       </Flex>
-    </>
+    </Flex>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,25 @@
 import React from "react"
+import { RouterProvider, createHashRouter } from "react-router-dom"
 import { ChakraProvider } from "@chakra-ui/react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
+import Article from "./pages/Article"
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/a/:id",
+    element: <Article />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
